9249fb1b7fa81024c7e91044a840975fa6bad542
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-06-11 12:13:53 -0700

    added a class to represent points and lines with a function for drawing a line of a given fill_color

--- src/graphics.py
+++ src/graphics.py
@@ -21,3 +21,24 @@ class Window():
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.__p1.x, 
+                           self.__p1.y, 
+                           self.__p2.x, 
+                           self.__p2.y, 
+                           fill=fill_color, 
+                           width=2)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
-    win = Window(800, 600)
+    max_width = 800
+    max_height = 600
+
+    win = Window(max_width, max_height)
+
+    line = Line(Point(0, 0), Point(max_width, max_height))
+    win.draw_line(line, "red")
+
     win.wait_for_close()
 
 main()
