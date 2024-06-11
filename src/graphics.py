360cbaa475219243a2530ab12fca4a1cf5ae4c78
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-06-11 13:00:27 -0700

    added a class to represent cells in a maze and a function for cells to draw themselves

--- src/graphics.py
+++ src/graphics.py
@@ -42,3 +42,33 @@ class Line():
                            self.__p2.y, 
                            fill=fill_color, 
                            width=2)
+
+class Cell():
+    def __init__(self, win, p1, p2):
+        self.__win = win
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__p_upper_l = p1
+        self.__p_lower_r = p2
+
+    def draw(self):
+        if self.has_left_wall or self.has_bottom_wall:
+            p_lower_l = Point(self.__p_upper_l.x,
+                          self.__p_lower_r.y)
+            if self.has_left_wall:
+                line = Line(self.__p_upper_l, p_lower_l)
+                self.__win.draw_line(line, "black")
+            if self.has_bottom_wall:
+                line = Line(p_lower_l, self.__p_lower_r)
+                self.__win.draw_line(line, "black") 
+        if self.has_right_wall or self.has_top_wall:
+            p_upper_r = Point(self.__p_lower_r.x, 
+                          self.__p_upper_l.y)
+            if self.has_right_wall:            
+                line = Line(p_upper_r, self.__p_lower_r)
+                self.__win.draw_line(line, "black")
+            if self.has_top_wall:
+                line = Line(self.__p_upper_l, p_upper_r)
+                self.__win.draw_line(line, "black")
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
     max_width = 800
@@ -6,8 +6,37 @@ def main():
 
     win = Window(max_width, max_height)
 
-    line = Line(Point(0, 0), Point(max_width, max_height))
-    win.draw_line(line, "red")
+    cells = []
+
+    for i in range(10, 210, 20):
+        cell = Cell(win, Point(i, i), Point(i + 10, i + 10))
+
+        n = len(cells)
+        if n == 0:
+            cell.has_top_wall = False
+        elif n == 1:
+            cell.has_bottom_wall = False
+        elif n == 2:
+            cell.has_left_wall = False
+        elif n == 3:
+            cell.has_right_wall = False
+        elif n == 4:
+            cell.has_top_wall = cell.has_bottom_wall = False
+        elif n == 5:
+            cell.has_right_wall = cell.has_left_wall = False
+        elif n == 6:
+            cell.has_right_wall = cell.has_top_wall = False
+        elif n == 7:
+            cell.has_right_wall = cell.has_bottom_wall = False
+        elif n == 8:
+            cell.has_left_wall = cell.has_top_wall = False
+        else:
+            cell.has_left_wall = cell.has_bottom_wall = False
+
+        cells.append(cell)
+
+    for cell in cells:
+        cell.draw() 
 
     win.wait_for_close()
 
