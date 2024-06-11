5e9f34ba4e7ed3bffb4a9703346ae733c8a31f81
Nick Langhorne <nicholaslanghorne@gmail.com>
2024-06-11 15:35:48 -0700

    added the maze class and functions to facilitate generating a maze

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from graphics import Window, Point, Line 
 from cell import Cell
+from maze import Maze
 
 def main():
     max_width = 800
@@ -7,43 +8,7 @@ def main():
 
     win = Window(max_width, max_height)
 
-    cells = []
-
-    for i in range(10, 210, 20):
-        cell = Cell(win, Point(i, i), Point(i + 10, i + 10))
-
-        n = len(cells)
-        if n == 0:
-            cell.has_top_wall = False
-        elif n == 1:
-            cell.has_bottom_wall = False
-        elif n == 2:
-            cell.has_left_wall = False
-        elif n == 3:
-            cell.has_right_wall = False
-        elif n == 4:
-            cell.has_top_wall = cell.has_bottom_wall = False
-        elif n == 5:
-            cell.has_right_wall = cell.has_left_wall = False
-        elif n == 6:
-            cell.has_right_wall = cell.has_top_wall = False
-        elif n == 7:
-            cell.has_right_wall = cell.has_bottom_wall = False
-        elif n == 8:
-            cell.has_left_wall = cell.has_top_wall = False
-        else:
-            cell.has_left_wall = cell.has_bottom_wall = False
-
-        cells.append(cell)
-
-    for cell in cells:
-        cell.draw() 
-
-    for i in range(5, 0, -1):
-        cells[i].draw_move(cells[i - 1])
-
-    for i in range(5, 9):
-        cells[i].draw_move(cells[i + 1], undo=True)
+    maze = Maze(Point(10, 10), 10, 10, 50, 50, win)
 
     win.wait_for_close()
 
---
+++ src/maze.py
@@ -0,0 +1,40 @@
+import time
+from graphics import Point
+from cell import Cell
+
+class Maze():
+    def __init__(self,
+                 upper_left_point,
+                 num_rows,
+                 num_cols,
+                 cell_size_x,
+                 cell_size_y,
+                 win):
+        self.__upper_left_point = upper_left_point
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self._create_and_draw_cells()
+
+    def _create_and_draw_cells(self):
+        for x in range(1, self.__num_cols + 1):
+            self.__cells.append([])
+            lower_right_x = self.__upper_left_point.x + (self.__cell_size_x * x)
+            upper_left_x = self.__upper_left_point.x + (self.__cell_size_x * (x - 1))
+            for y in range(1, self.__num_rows + 1):
+                upper_left_y = self.__upper_left_point.y + (self.__cell_size_y * (y - 1))
+                upper_left_point = Point(upper_left_x, upper_left_y)
+                lower_right_y = self.__upper_left_point.y + self.__cell_size_y * y
+                lower_right_point = Point(lower_right_x, lower_right_y)
+                cell = Cell(self.__win, upper_left_point, lower_right_point)
+                self.__cells[x - 1].append(cell)
+                cell.draw()
+                self.__win.redraw()
+                self._animate()
+
+    def _animate(self):
+        self.__win.redraw()
+        time.sleep(0.05)
